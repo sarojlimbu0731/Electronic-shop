9e286401ff4be1aaf170e5829b777c5cb7950157
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-07-01 18:29:37 +0545

    create  the sidebar of card components

--- client/src/Components/Cart/Cart.js
+++ client/src/Components/Cart/Cart.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AiOutlinePlus, AiOutlineMinus, AiTwotoneDelete } from "react-icons/ai";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const { products,totalqty,totalprice } = useSelector((state) => state.cartReducer);
+  console.log(products);
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart-box">
+      <div className="container">
+        <h3>Your Cart</h3>
+        {products.length > 0 ? (
+          <>
+            {products.map((product) => (
+              <div className="cart-row">
+                <div className="cart-col1">
+                  <img src="#" alt={product.name} />
+                </div>
+                <div className="cart-col2">
+                  <span>{product.name}</span>
+                  <span>{product.totalqty}</span>
+                  <span>{product.quantity*product.price}</span>
+                </div>
+                <div className="cart-col3">
+                  <div>
+                    <AiOutlineMinus onClick={()=>dispatch({type:"DEC",payload:product.id})}/>
+                    <strong>{product.quantity}</strong>
+                    <AiOutlinePlus onClick={()=>dispatch({type:"INC",payload:product.id})} />
+                  </div>
+                </div>
+                <div>
+                  <AiTwotoneDelete  onClick={()=> dispatch({type:"REMOVE",payload:product.id})}/>
+                </div>
+    
+              </div>
+            ))}
+                        <div className="checkout">
+                    <span>total price:-{totalprice}</span>
+                    <span>total quantity:-{totalqty}</span>
+                    <button>checkout</button>
+                </div>
+ 
+          </>
+        ) : (
+          "Your cart is empty"
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
--- client/src/store/reducer/CartReducer.js
+++ client/src/store/reducer/CartReducer.js
@@ -1,34 +1,65 @@
-const initstate= {
-    products:[],
-    totalprice:0,
-    totalqty:0,
-}
+const initstate = {
+  products: [],
+  totalprice: 0,
+  totalqty: 0,
+};
 
-const cartReducer=(state=initstate, action) =>{
-         switch(action.type){
-            case "ADD_TO_CART":
-                const {product,quantity} =action.payload;
-                
-                const check= state.products.find(item=> item.id === product.id)
-                if (check){
-                    return state
-                }
-                else{
-                    const tprice= state.totalprice + quantity* product.price
-                    
+const cartReducer = (state = initstate, action) => {
+    let findpro;
+    let index;
+  switch (action.type) {
+    case "ADD_TO_CART":
+      const { product, quantity } = action.payload;
 
-                     product.stock= product.stock-quantity
-                    
-                  
-                    const tqty= state.totalqty +quantity
-                    product.quantity=quantity
-                    return {
-                        ...state, products:[...state.products,product],totalprice:tprice, totalqty:tqty
-                    }
-                }
-            default:
-                return state
-         }
-}
+      const check = state.products.find((item) => item.id === product.id);
+      if (check) {
+        return state;
+      } else {
+        const tprice = state.totalprice + quantity * product.price;
+        const tqty = state.totalqty + quantity;
+        product.quantity = quantity;
+        return {
+          ...state,
+          products: [...state.products, product],
+          totalprice: tprice,
+          totalqty: tqty,
+        };
+      }
+    case "INC":
+        findpro= state.products.find(product=>product.id === action.payload);
+        index = state.products.findIndex(product=> product.id === action.payload);
+        findpro.quantity +=1
+        state.products[index]= findpro;
+        return {
+            ...state,
+            totalprice:state.totalprice + findpro.price,
+            totalqty:state.totalqty+ 1
+        }
+    case "DEC":
+        findpro=state.products.find(product=>product.id === action.payload)
+        index= state.products.findIndex(product=>product.id=== action.payload)
+      if (findpro.quantity>1){
+        findpro.quantity -=1
+        state.products[index]= findpro
+      }
+      return {
+        ...state,
+        totalprice: state.totalprice -findpro.price,
+        totalqty:state.totalqty -1
+      }
+    case "REMOVE":
+        findpro=state.products.find(product=>product.id === action.payload)
+        const filtered =state.products.filter(product=> product.id !== action.payload)
+        return {
+            ...state,
+            products:filtered,
+            totalprice:state.totalprice- findpro.quantity*findpro.price,
+            totalqty:state.totalqty- findpro.quantity
+        }
 
-export default cartReducer
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
