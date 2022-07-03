9838182f79b51b39952c290576c86376e8fedfcb
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-07-03 12:00:44 +0545

    styling using sass

--- client/src/Components/Navbar/Nav.js
+++ client/src/Components/Navbar/Nav.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import CartModel from "../CartSidebar/CartSidebar";
+import "../../dist/Navbar.css";
 
 // import { Button, Menu } from '@mantine/core';
 
@@ -11,45 +12,36 @@ const Nav = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const { totalqty } = useSelector((state) => state.cartReducer);
-  const [chooseproduct, setproduct] = useState("");
 
   const handleClick = () => {
     setModalOpen(true);
   };
 
   const handleproduct = (e) => {
-    if (e.target.value === "all")
-    {
-      dispatch({type:"UPDATE_PRODUCT"})
-    }
-    else{
+    if (e.target.value === "all") {
+      dispatch({ type: "UPDATE_PRODUCT" });
+    } else {
       dispatch({ type: "CHECK_PRODUCT", payload: e.target.value });
     }
-  
   };
 
-
   return (
-    <div className="nav">
+    <div >
       <CartModel modalOpen={modalOpen} setModalOpen={setModalOpen} />
-      <div className="container">
-        <div className="nav_container">
-          <div className="nav_left">
+      {/* <div className="container"> */}
+      <div className="nav">
+          <div>
             <Link to="/">
-              {" "}
               <h2 onClick={() => dispatch({ type: "UPDATE_PRODUCT" })}>
                 Electronic Shop
               </h2>
             </Link>
           </div>
-          <div className="nav-right">
-            <span className="shopping-cart">
-              <strong>Cart</strong>
-              <AiOutlineShoppingCart onClick={handleClick} />
+          <div >
+              <AiOutlineShoppingCart className="cartlogo" onClick={handleClick} />
               <strong>{totalqty}</strong>
-            </span>
           </div>
-       
+
           <div>
             <label for="product">
               <strong>Category</strong>
@@ -64,14 +56,15 @@ const Nav = () => {
               <option value="keyboard">Keyboard</option>
             </select>
           </div>
+
+         
           <div>
             <Link to="/checkout">Checkout</Link>
           </div>
         </div>
-      </div>
+
     </div>
   );
 };
 
 export default Nav;
-
