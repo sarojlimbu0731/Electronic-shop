3f3e8c173b5b0bf37b94ed7e83b5e9e3b310fe89
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-07-03 10:26:09 +0545

    add of the filter functionality in the project

--- client/src/Components/Navbar/Nav.js
+++ client/src/Components/Navbar/Nav.js
@@ -1,39 +1,77 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { useSelector } from 'react-redux';
-import CartModel from '../CartSidebar/CartSidebar';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import CartModel from "../CartSidebar/CartSidebar";
+
+// import { Button, Menu } from '@mantine/core';
 
 const Nav = () => {
-    const [modalOpen,setModalOpen]= useState(false)
+  const dispatch = useDispatch();
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const {totalqty}=useSelector(state=> state.cartReducer)
+  const { totalqty } = useSelector((state) => state.cartReducer);
+  const [chooseproduct, setproduct] = useState("");
 
-
-  const handleClick=()=>{
-      setModalOpen(true)
+  const handleClick = () => {
+    setModalOpen(true);
   };
 
-  return (  
-    <div className="nav">
-            <CartModel  modalOpen={modalOpen} setModalOpen={setModalOpen} />
-        <div className='container'>
-            <div className='nav_container'>
-                <div className='nav_left'>
-                <Link to ="/"> <h2>Electronic Shop</h2></Link>
-                </div>
-                <div className='nav-right'>
-                  <span className='shopping-cart'><strong>Cart</strong><AiOutlineShoppingCart  onClick={handleClick} /><strong>{totalqty}</strong></span>  
-                </div>
-                <div>
-                    <Link to ="/checkout">Checkout</Link>
-                </div>
-            </div>
-        </div>
-        
-    </div>
+  const handleproduct = (e) => {
+    if (e.target.value === "all")
+    {
+      dispatch({type:"UPDATE_PRODUCT"})
+    }
+    else{
+      dispatch({ type: "CHECK_PRODUCT", payload: e.target.value });
+    }
+  
+  };
 
-  )
-}
+
+  return (
+    <div className="nav">
+      <CartModel modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <div className="container">
+        <div className="nav_container">
+          <div className="nav_left">
+            <Link to="/">
+              {" "}
+              <h2 onClick={() => dispatch({ type: "UPDATE_PRODUCT" })}>
+                Electronic Shop
+              </h2>
+            </Link>
+          </div>
+          <div className="nav-right">
+            <span className="shopping-cart">
+              <strong>Cart</strong>
+              <AiOutlineShoppingCart onClick={handleClick} />
+              <strong>{totalqty}</strong>
+            </span>
+          </div>
+       
+          <div>
+            <label for="product">
+              <strong>Category</strong>
+            </label>
+
+            <select id="product" onChange={handleproduct}>
+              <option value="all">All</option>
+              <option value="laptop">Laptop</option>
+              <option value="mobile">Mobile</option>
+              <option value="watch">Watch</option>
+              <option value="headseat">Headset</option>
+              <option value="keyboard">Keyboard</option>
+            </select>
+          </div>
+          <div>
+            <Link to="/checkout">Checkout</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
+
--- client/src/store/reducer/ProductReducer.js
+++ client/src/store/reducer/ProductReducer.js
@@ -1,43 +1,61 @@
-const initState={ 
-products:[],
-product:{ },
-}
+const initState = {
+  products: [],
+  product: [],
+};
 
+const productReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "SET_PRODUCT":
+      let products = action.payload;
+      products.forEach((product) => {
+        let date = product.createDate;
+        let day, month, yrs;
+        date = new Date(date);
+        date = JSON.stringify(date);
+        yrs = date.slice(1, 5);
+        month = date.slice(6, 8);
+        day = date.slice(9, 11);
+        date = day + "-" + month + "-" + yrs;
+        product.createDate = date;
+        product.price = parseInt(product.price.slice(1)) * 126;
+      });
 
+      return {
+        ...state,
+        products: [...products],
+      };
+    case "CHECK_PRODUCT":
+      if (state.products.length > state.product.length) {
+        state.product = state.products;
+      } else {
+        state.products = state.product;
+      }
+      const filtered = state.product.filter(
+        (product) => product.category[1] === action.payload
+      );
 
-const productReducer = (state= initState,action)=>{
+      return {
+        ...state,
+        products: [...filtered],
+      };
 
-    switch(action.type){
+    case "UPDATE_PRODUCT":
+      return {
+        ...state,
+        products: [...state.product],
+      };
 
-        case "SET_PRODUCT":
-            let products =action.payload;
-            products.forEach(product => {
-                let date=product.createDate
-                let day,month,yrs
-                date=new Date(date)
-                date=JSON.stringify(date)
-                yrs=date.slice(1,5)
-                month=date.slice(6,8)
-                day=date.slice(9,11)
-                date=day+"-"+month+"-"+yrs
-                product.createDate=date
-                product.price= parseInt(product.price.slice(1))*126
-            });
-         
-            return {
-               
-                ...state,products:[...products]
-            }
-          
-     
-        case "PRODUCT":
-             
-            return {...state,product:state.products.find(product=>product.id === parseInt(action.payload) )}
+    case "PRODUCT":
+      return {
+        ...state,
+        product: state.products.find(
+          (product) => product.id === parseInt(action.payload)
+        ),
+      };
 
-        default:
-            return state
-    }
-    
-}
+    default:
+      return state;
+  }
+};
 
 export default productReducer;
